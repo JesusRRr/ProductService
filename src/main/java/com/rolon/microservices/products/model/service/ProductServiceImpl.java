82db563aaf3e52865daea3dfc35a11a8da4e03a5
JesusRRr <rolonjesus1996@gmail.com>
2021-02-15 17:35:04 -0600

    chore: using jackson dependency in deleteById method in ProductSetviceImpl

--- src/main/java/com/rolon/microservices/products/model/service/ProductServiceImpl.java
+++ src/main/java/com/rolon/microservices/products/model/service/ProductServiceImpl.java
@@ -1,11 +1,15 @@
 package com.rolon.microservices.products.model.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rolon.microservices.products.model.entity.Product;
 import com.rolon.microservices.products.model.repository.ProductRepository;
 
@@ -31,12 +35,19 @@ public class ProductServiceImpl implements IProductService{
 	@Transactional(readOnly= true)
 	public String deleteById(Long id) {
 		Product product = findById(id);
-		productoRepository.deleteById(id);
-		return new StringBuilder("{\"message\"")
-				.append(":")
-				.append("\"")
-				.append(product.getName())
-				.append(" deleted\"}")
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, String> map = new HashMap();
+		String message = new StringBuilder(product.getName())
+				.append(" deleted")
 				.toString();
+		
+		map.put("message", message);
+		try {
+			return objectMapper.writeValueAsString(map);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+
 	}
 }
